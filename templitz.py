2c7d19f8ff8e9693022f44d01f86f1d53c21bf32
Caleb Hattingh <caleb.hattingh@gmail.com>
2017-10-17 16:57:56 +1000

    Moved default templitz to a library dir.

--- templitz.py
+++ templitz.py
@@ -17,50 +17,76 @@ def file_or_stdout(args):
     if args.stdout:
         f = sys.stdout
     else:
-        f = open(args.template.rpartition('.')[0], 'w+')
+        # Remove the trailing ".templitz"
+        fname = args.template.rpartition('.')[0]
+        target = os.path.join(args.outdir, target)
+        f = open(target, 'w+')
     try:
         yield f
     finally:
         if f is not sys.stdout:
             f.close()
 
-
-def subs(args):
+def load_template(args):
     paths = biodome.environ.get('TEMPLITZ_PATH', '').split(os.pathsep)
-    # Current dir first, and dir of templitz.py as last resort
-    paths = chain([os.getcwd()], paths, [os.path.dirname(__file__)])
+    # Current dir first, and /library of templitz.py dir as last resort
+    paths = chain(
+        [os.getcwd()],
+        paths,
+        [os.path.join(os.path.dirname(__file__), 'library')]
+    )
     possibles = (os.path.join(p, args.template) for p in paths)
     for hit in possibles:
         if os.path.exists(hit):
             break
     else:
-        print(f'Error: template "{args.template}" not found in any of '
-              f'the following locations:')
-        for p in paths:
-            print(f'    {p}')
-        return
+        msg = (f'Error: template "{args.template}" not found in any of '
+               f'the following locations:')
+        msg += '\n'.join(paths)
+        raise FileNotFoundError(msg)
 
     with open(hit) as f:
         data = f.read()
 
-    tmpl = Template(data)
-    print('The template has the following vars: ')
-    for ph in tmpl.placeholders():
-        print(ph)
-        print('   ${%s}' % ph)
+    return Template(data)
 
+
+def subs(args):
+    tmpl = load_template(args)
     output = tmpl.safe_substitute({})
-
     with file_or_stdout(args) as f:
         f.write(output)
 
 
+def info(args):
+    tmpl = load_template(args)
+    print('The template has the following vars: ')
+    print()
+    for ph in tmpl.placeholders():
+        print('   ${%s}' % ph)
+    print()
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--template', required=True)
-    parser.add_argument('-s', '--stdout', action='store_true')
+    parser.add_argument('-t', '--templit', required=True)
+    parser.add_argument('-i', '--info', help='Information about the templit.',
+                        action='store_true')
+    parser.add_argument('-l', '--list', help='List all available templitz.' )
+    parser.add_argument('-s', '--stdout', action='store_true',
+                        help='Write to stdout instead of file.')
+    parser.add_argument('-o', '--outdir', help='Output directory.',
+                        default=os.getcwd())
     args = parser.parse_args()
-    subs(args)
+    try:
+        if args.info:
+            info(args)
+        elif args.list:
+            raise NotImplementedError
+        else:
+            subs(args)
+    except FileNotFoundError as e:
+        print(f'Error: {e!}')
 
 
 if __name__ == '__main__':
